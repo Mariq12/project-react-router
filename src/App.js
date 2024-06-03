23ddab94a39b1d333edd2f4bc47eea3031b0b15f
Mariq12 <fmlabanda@utpl.edu.ec>
2024-06-03 11:33:33 -0500

    Se agregaron las rutas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './styles/base/base.css';
 import './styles/components/card.css';
+import About from './pages/About';
+import Home from './pages/Home';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
