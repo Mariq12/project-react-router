ec9f1958665d6c82d42c6c95d3379235c8299386
Mariq12 <fmlabanda@utpl.edu.ec>
2024-06-03 13:39:49 -0500

    Cambio de la etiqueta a por componente Link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import image from "../assets/img/doguito.svg"
 import "../styles/components-style/header.css"
-
+import { Link } from "react-router-dom"
 
 const Header = () => {
     return (
@@ -9,17 +9,17 @@ const Header = () => {
                 <span className="menu-hamburguer__icon"></span>
             </div>
             <div className="header-container">
-                <a href="/" className="flex flex--center">
+                <Link to="/" className="flex flex--center">
                     <img className="header__logo" src={image} alt="doguito" />
                     <h1 className="header__title">Petshop</h1>
-                </a>
+                </Link>
             </div>
             <nav className="menu-header">
                 <ul className="menu-items">
-                    <li><a className="menu-item menu-item--entrar" href="#">Entrar</a></li>
-                    <li><a className="menu-item" href="#">Productos</a></li>
-                    <li><a className="menu-item" href="/">Blog</a></li>
-                    <li><a className="menu-item" href="/sobre">Sobre</a></li>
+                    <li><Link className="menu-item menu-item--entrar" to="#">Entrar</Link></li>
+                    <li><Link className="menu-item" to="#">Productos</Link></li>
+                    <li><Link className="menu-item" to="/">Blog</Link></li>
+                    <li><Link className="menu-item" to="/about">Sobre</Link></li>
                 </ul>
             </nav>
             <div className="menu-header-background"></div>
