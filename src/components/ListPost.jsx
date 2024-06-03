7e9e9424701520a0b0d78c75e2edb0854fb2a644
Mariq12 <fmlabanda@utpl.edu.ec>
2024-06-03 15:09:23 -0500

    Nuevo archivo creado

---
+++ src/components/ListPost.jsx
@@ -0,0 +1,11 @@
+import "../styles/components-style/card.css";
+
+const ListPost = ({ url }) => {
+    return (
+        <section className="posts container">
+
+        </section>
+    );
+}
+
+export default ListPost;
