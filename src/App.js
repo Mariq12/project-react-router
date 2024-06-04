234207d3f5796b0e7da6f19f1f805599856b199f
Mariq12 <fmlabanda@utpl.edu.ec>
2024-06-03 19:15:03 -0500

    Se aplica concepto de rutas anidadas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './styles/base/base.css';
 import './styles/components-style/card.css';
-import About from './pages/About';
+import Header from './components/Header';
 import Home from './pages/Home';
+import About from './pages/About';
 import Post from './pages/Post';
+import Category from './pages/Category';
+import Page404 from './pages/Page404';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Page404 from './pages/Page404';
-import Header from './components/Header';
+
+
 
 function App() {
   return (
@@ -16,6 +19,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/posts/:id" element={<Post />} />
+        <Route path="/category/:id" element={<Category />} />
         <Route path="*" element={<Page404 />} />
       </Routes>
     </Router>
---
+++ src/pages/Category.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect} from "react";
+import "../styles/blog.css";
+import { search } from "../api/api";
+import ListCategories from "../components/ListCategories";
+import ListPost from "../components/ListPost";
+import SubCategory from "../components/SubCategory";
+import { useParams, Routes, Route, Link, useResolvedPath } from "react-router-dom";
+
+const Category = () => {
+    const [subcategories, setSubcategories] = useState([]);
+    const { id } = useParams();
+
+    const url = useResolvedPath("").pathname
+
+    useEffect(() => {
+        search(`/categories?id=${id}`, (response) => {
+            setSubcategories(response[0].subcategories)
+        } )
+    }, [id])
+
+    return (
+        <>
+            <div className="container">
+                <h2 className="title-page">Pet noticias</h2>
+            </div>
+            <ListCategories />
+            <ul className="category-list container flex">
+                {
+                    subcategories.map(subcategory => (
+                        <li className={`category-list__category category-list__category--${id}`} key={subcategory}>
+                                <Link to={`${url}/${subcategory}`}>
+                                {subcategory}
+                            </Link>
+                        </li>
+                    ))
+                }
+            </ul>
+            <Routes>
+                <Route path="/" element={<ListPost url={`/posts?category=${id}`} />} />
+                <Route path="/:subcategory" element={<SubCategory />} />
+            </Routes>
+        </>
+    );
+}
+
+export default Category;
